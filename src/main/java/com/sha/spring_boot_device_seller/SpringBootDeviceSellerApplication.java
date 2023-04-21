5cfb1c462daed18994ce3eb6db44d6343fbe10f0
Duy Nguyen <hding2196@gmail.com>
2023-04-21 21:22:42 +0900

    first commit

---
+++ src/main/java/com/sha/spring_boot_device_seller/SpringBootDeviceSellerApplication.java
@@ -0,0 +1,13 @@
+package com.sha.spring_boot_device_seller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDeviceSellerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDeviceSellerApplication.class, args);
+	}
+
+}
