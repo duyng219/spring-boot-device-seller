8a24c785c02b1952f896184eee13aefe3536f5ce
Duy Nguyen <hding2196@gmail.com>
2023-04-21 22:56:42 +0900

    update 21-04

---
+++ src/main/java/com/sha/spring_boot_device_seller/model/Device.java
@@ -0,0 +1,32 @@
+package com.sha.spring_boot_device_seller.model;
+
+import lombok.Data;
+import org.hibernate.procedure.spi.ParameterRegistrationImplementor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "devices")
+public class Device {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
+
+    @Column(name = "description", nullable = false, length = 100)
+    private String description;
+
+    @Column(name = "price", nullable = false)
+    private double price;
+
+    @Column(name = "create_time", nullable = false)
+    private LocalDateTime createTime;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "deviceType", nullable = false)
+    private DeviceType deviceType;
+}
